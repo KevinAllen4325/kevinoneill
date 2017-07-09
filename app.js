cf094eb40bd30365ecd6c8130fead73d4fc91997
Kevin O'Neill <kevinallenoneill@gmail.com>
2017-07-09 10:50:20 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-	if (window.innerWidth < 769){
-		
+	if (window.innerWidth < 769) {
 		document.getElementById("nav").classList.remove("w3-animate-opacity");
 		document.getElementById("nav").classList.add("w3-animate-top");
 	}
